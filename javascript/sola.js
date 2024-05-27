37aabfc56cd06c1899044090cac430a653d922ac
JhonSailor98 <jddios98@hotmail.com>
2024-05-27 13:41:36 -0600

    Update sola.js

--- javascript/sola.js
+++ javascript/sola.js
@@ -3,7 +3,7 @@
             Frases3[0] = "Recuerda que tienes guarado en whatsapp a alguien que le pusiste mi preciosote, siempre podrás hablar con él";
             Frases3[1] = "Tienes un contacto en instagram (osea yo), y también puedes hablarme por esta red social";
             Frases3[2] = "A veces sentirse solo te hace cuestionarte quien está contigo y aprendes a conocerte a ti misma";
-            Frases3[3] = "Ciertas personas no están listas para concoer a otras.";
+            Frases3[3] = "Ciertas personas no están listas para concocer a otras.";
             Frases3[4] = "​Siempre tendrás a familia y amigos que te van a apoyar, aunque no siempre estén cerca de ti";
             Frases3[5] = "Mandame mensaje :) estaré para ti y te escucharé";
             var Q3 = Frases3.length;
