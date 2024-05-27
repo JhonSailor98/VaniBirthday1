b7eb2c38fe336fb6c98d9444b82f7eddd7e9148c
JhonSailor98 <jddios98@hotmail.com>
2024-05-27 12:30:09 -0600

    Add files via upload

---
+++ triste.js
@@ -0,0 +1,28 @@
+           
+            var Frases6=new Array()
+            Frases6[0] = "Cuando sientas que el mundo se te viene encima, recuerda que aquí estaré siempre para apoyarte";
+            Frases6[1] = "Te contaré un chiste, puede que te ayude jeje, ¿Cómo se llama el primo vegano de Bruce Lee? Broco Lee, está malo lo sé jaja";
+            Frases6[2] = "Te contaré un chiste, puede que te ayude jeje, ¿Qué le dice un ganso a una gansa? ¡Vengansa!, está malo lo sé jaja";
+            Frases6[3] = "Te contaré un chiste, puede que te ayude jeje, ¿Qué le dice un jardinero a otro? Nos vemos cuando podamos, está malo lo sé jaja";
+            Frases6[4] = "Te contaré un chiste, puede que te ayude jeje, El otro día vendí mi aspiradora. Lo único que hacía era acumular polvo, está malo lo sé jaja​";
+            Frases6[5] = "Te contaré un chiste, puede que te ayude jeje, ¿Qué es rojo y tiene forma de cubo? Un cubo azul pintado de rojo, está malo lo sé jaja";
+            Frases6[6] = "Te contaré un chiste, puede que te ayude jeje, Van dos fantasmas y se cae el del médium, está malo lo sé jaja​";
+            Frases6[7] = "Te contaré un chiste, puede que te ayude jeje, ¿Qué hace una abeja en un gimnasio? Zumba., perdona este si es malisimo jaja​";
+            Frases6[8] = "Te contaré un chiste, puede que te ayude jeje, Si se muere una pulga, ¿dónde va? Al pulgatorio, está malo lo sé jaja";
+            Frases6[9] = "Te contaré un chiste, puede que te ayude jeje, ¿Qué le dice un techo a otro? Techo de menos, como yo te hecho de menos a ti mi amor :(";
+            Frases6[10] = "Te contaré un chiste, puede que te ayude jeje, ¿Por qué llora un libro de matemáticas? ¡Porque tiene muchos problemas!, está malo lo sé jaja";
+            Frases6[11] = "Te contaré un chiste, puede que te ayude jeje, ¿Qué tipo de coche lleva Santa Claus? ¡Un Renol!, este es especial para ti bb que te gustan los carros jaja";
+            Frases6[12] = "Te contaré un chiste, puede que te ayude jeje, Una vez conté un chiste químico, pero no hubo reacción, está malo lo sé jaja​";
+            Frases6[13] = "Te contaré un chiste, puede que te ayude jeje, Un león se comió un jabón. Y ahora es puma, está malo lo sé jaja";
+            Frases6[14] = "Te contaré un chiste, puede que te ayude jeje, ¿Dónde vive Iron Man? Iron Know, está malo lo sé jaja";
+            Frases6[15] = "Te contaré un chiste, puede que te ayude jeje, Los hippies no orinan: hacen peace, está malo lo sé jaja";
+            Frases6[16] = "Te contaré un chiste, puede que te ayude jeje, No cabe duda, y duda se fue caminando, está malo lo sé jaja​";
+            Frases6[17] = "Te contaré un chiste, puede que te ayude jeje, ¿De dónde vienen los hámster? De Hamsterdam, ahí vamos a ir algún día jeje";
+            Frases6[18] = "Cuando estés triste me esforzaré por sacarte una sonrisa";
+            Frases6[19] = "Mi amor, me encanta cuando sonries, por favor no dejes de hacerlo";
+            var Q6 = Frases6.length;
+
+            var numAleatorio6=Math.round(Math.random()*(Q6-1));
+
+            function mostrarFrases6() {
+            document.write(Frases6[numAleatorio6]);}
